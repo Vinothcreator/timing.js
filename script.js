002c50bcb45fdb9e693d587ec6b1b0f4ea1bf1a8
Vinoth <vinoth542005@gmail.com>
2024-07-03 12:46:55 +0530

    Add files via upload
    when a Global timing can display with the coding
    Using SetInterval( ), SetTiming( ) can used

---
+++ script.js
@@ -0,0 +1,18 @@
+// function add(num1){
+//     console.log(num);
+//     if(num<=10){
+//         add(num++)
+//     }
+    
+// }
+// add(1)
+function start(){
+    console.log("Start");
+}
+//setTimeout(start,5000)
+setInterval(start,1000)
+                                                        //multithreading fuction
+function running(){
+    console.log("running");
+}
+running()
